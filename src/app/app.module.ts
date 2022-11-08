22e29626c48f8cd0e6625dedf93d1cea3f8926e6
Kevin Rodrigues <kevinpatrickrj@gmail.com>
2022-11-07 22:17:23 -0300

    atividade 4 atualizado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FotoComponent } from './foto/foto.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FotoComponent
+    FotoComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
